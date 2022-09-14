0b52db244c2723e136d082ad2f7c51ff986a8e69
yksolanki9 <yashsolankli1709@gmail.com>
2022-09-14 21:02:23 +0530

    Send event from plugin when a file is shared

--- android/src/main/java/com/mycompany/plugins/example/ExamplePlugin.java
+++ android/src/main/java/com/mycompany/plugins/example/ExamplePlugin.java
@@ -1,5 +1,9 @@
 package com.mycompany.plugins.example;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -9,14 +13,31 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 @CapacitorPlugin(name = "Example")
 public class ExamplePlugin extends Plugin {
 
-    private Example implementation = new Example();
+//    private Example implementation = new Example();
+//
+//    @PluginMethod
+//    public void echo(PluginCall call) {
+//        String value = call.getString("value");
+//
+//        JSObject ret = new JSObject();
+//        ret.put("value", implementation.echo(value));
+//        call.resolve(ret);
+//    }
 
-    @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    @Override
+    public void load() {
+        Intent intent = new Intent();
+        String intentAction = intent.getAction();
+        String intentType = intent.getType();
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+        if(Intent.ACTION_SEND.equals(intentAction) && intentType.startsWith("image")) {
+            Log.d("test", intentType);
+            Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+            JSObject ret = new JSObject();
+            ret.put("imageUri", imageUri);
+            notifyListeners("imageShared", ret);
+        }
+        getActivity().startActivity(intent);
     }
+
 }
