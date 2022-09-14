9c60ffae7ec111e0fc16a790cbd6af86d6ecd438
yksolanki9 <yashsolankli1709@gmail.com>
2022-09-14 22:15:26 +0530

    Fix logic for passing data to app component

--- android/src/main/java/com/mycompany/plugins/example/ExamplePlugin.java
+++ android/src/main/java/com/mycompany/plugins/example/ExamplePlugin.java
@@ -2,6 +2,7 @@ package com.mycompany.plugins.example;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Base64;
 import android.util.Log;
 
 import com.getcapacitor.JSObject;
@@ -9,6 +10,10 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @CapacitorPlugin(name = "Example")
 public class ExamplePlugin extends Plugin {
@@ -24,20 +29,46 @@ public class ExamplePlugin extends Plugin {
 //        call.resolve(ret);
 //    }
 
-    @Override
-    public void load() {
-        Intent intent = new Intent();
-        String intentAction = intent.getAction();
-        String intentType = intent.getType();
+//    @Override
+//    public void load() {
+////        Intent intent = new Intent();
+////
+////        String intentAction = intent.getAction();
+////        String intentType = intent.getType();
+////
+////        if(Intent.ACTION_SEND.equals(intentAction) && intentType.startsWith("image")) {
+////            Log.d("test", intentType);
+////            Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+////            JSObject ret = new JSObject();
+////            ret.put("imageUri", imageUri);
+////            notifyListeners("imageShared", ret);
+////
+////            Log.d("SOLANKI", "EVENT MENE FEK DIYA");
+////
+//////            getActivity().startActivity(intent);
+////        }
+//    }
 
-        if(Intent.ACTION_SEND.equals(intentAction) && intentType.startsWith("image")) {
-            Log.d("test", intentType);
-            Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
-            JSObject ret = new JSObject();
-            ret.put("imageUri", imageUri);
-            notifyListeners("imageShared", ret);
+    @Override
+    protected void handleOnNewIntent(Intent intent) {
+        super.handleOnNewIntent(intent);
+
+        Log.d("SOLANKI", "EVENT MENE FEK DIYA");
+
+        // read intent
+        String action = intent.getAction();
+//        Uri url = intent.getData();
+
+        Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+
+        if (!Intent.ACTION_SEND.equals(action) || imageUri == null) {
+            return;
         }
-        getActivity().startActivity(intent);
+
+        JSObject ret = new JSObject();
+        ret.put("url", imageUri.toString());
+        notifyListeners("imageShared", ret, true);
+
     }
 
 }
