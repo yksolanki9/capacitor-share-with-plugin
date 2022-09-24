3b5c4dd9db5930bd0a96b0e2a0531f7d5078133a
yksolanki9 <yashsolankli1709@gmail.com>
2022-09-24 20:18:31 +0530

    Remove logic for calculating and returning image base64

--- android/src/main/java/com/capacitor/plugins/sharewithplugin/ShareWithPlugin.java
+++ android/src/main/java/com/capacitor/plugins/sharewithplugin/ShareWithPlugin.java
@@ -2,17 +2,12 @@ package com.capacitor.plugins.sharewithplugin;
 
 import android.content.ContentResolver;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.util.Base64;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,26 +38,14 @@ public class ShareWithPlugin extends Plugin {
 
     public Map<String, String> getImageDataFromUri(Uri uri) {
         Map<String, String> imageData = new HashMap<>();
+        ContentResolver contentResolver = getContext().getContentResolver();
 
-        try {
-            ContentResolver contentResolver = getContext().getContentResolver();
+        String imageType = contentResolver.getType(uri);
+        String[] imageTypeArr = imageType.split("/");
+        String imageExt = imageTypeArr.length > 1 ? imageTypeArr[1] : "";
 
-            Bitmap bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri);
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-            byte[] bytes = stream.toByteArray();
-            String imageBase64 = "data:image/jpeg;base64," + Base64.encodeToString(bytes, Base64.DEFAULT);
-
-            String imageType = contentResolver.getType(uri);
-            String[] imageTypeArr = imageType.split("/");
-            String imageExt = imageTypeArr.length > 1 ? imageTypeArr[1] : "";
-
-            imageData.put("base64", imageBase64);
-            imageData.put("uri", uri.toString());
-            imageData.put("ext", imageExt);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        imageData.put("uri", uri.toString());
+        imageData.put("ext", imageExt);
 
         return imageData;
     }
